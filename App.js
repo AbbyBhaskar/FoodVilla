f3ae7049b98a778f4b23f71c0c370b8c7af1c5e6
AbbyBhaskar <abbybhaskar31@gmail.com>
2023-07-07 19:14:06 +0530

    First Ccommit

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I am h1 tag"),
@@ -8,5 +11,6 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I am h2 tag"),
   ]),
 ]);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
